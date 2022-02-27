54ed6b19b9c5870817a833271c747c97a07b1564
stevan1008 <stevan1008@hotmail.com>
2022-02-27 12:22:52 -0500

    Finished simple inputs and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teamApp';
+  newMemberName: string = "";
+  members: string[] = [];
+  errorMessage: string = "";
+  numberOfTeams: number | "" = "";
+  teams: string[][] = [[]]
+
+  onInput(member: string) {
+    this.newMemberName = member
+  }
+
+  onNumberOfTeamsInput(value: string) {
+    this.numberOfTeams = Number(value)
+  }
+
+  addMember(){
+    if(!this.newMemberName) {
+      this.errorMessage = "Name can't be empty"
+      return
+    }
+    this.errorMessage = ""
+    this.members.push(this.newMemberName)
+    this.newMemberName = ""
+  }
+
+  generateTeams() {
+    this.teams = [];
+    const allMembers = [...this.members];
+    if (this.members.length < this.numberOfTeams) {
+      this.errorMessage = 'Not enough members';
+      return;
+    }
+
+    this.errorMessage = '';
+    while(allMembers.length){
+      for (let i = 0; i < this.numberOfTeams; i++){
+        const randomIndex = Math.floor(Math.random() * allMembers.length)
+        const member = allMembers.splice(randomIndex, 1)[0]
+        if(!member) break
+        if(this.teams[i]){
+          this.teams[i].push(member)
+        }else {
+          this.teams[i] = [member]
+        }
+      }
+    }
+    this.members = [];
+    this.numberOfTeams = '';
+  }
 }
